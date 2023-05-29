904c3ed7d29a28ec9112fd3127a8660de9177ee3
Avoid <116073567+Warghto@users.noreply.github.com>
2023-05-29 11:30:24 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./styles.css";
+import Card from "./components/Card";
 
+// const url = "14288fe27e1df35d1f13e5fdfe9f7840"
+// https://api.themoviedb.org/3/movie/11?api_key=14288fe27e1df35d1f13e5fdfe9f7840
 export default function App() {
   return <div className="App"></div>;
 }
